7018a43a80f50a7654f759c1a611bc8548e55af7
Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
2012-05-10 16:21:47 +0200

    add documents.

---
+++ Document/Option/Option.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\AssortmentBundle\Document\Option;
+
+use Sylius\Bundle\AssortmentBundle\Model\Fluid\Option\Option as BaseOption;
+
+/**
+ * Option default document.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+class Option extends BaseOption
+{
+}
+
---
+++ Document/Variant/Variant.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Paweł Jędrzejewski
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Sylius\Bundle\AssortmentBundle\Document\Variant;
+
+use Sylius\Bundle\AssortmentBundle\Model\Fluid\Variant as BaseVariant;
+
+/**
+ * Variant default document.
+ *
+ * @author Paweł Jędrzejewski <pjedrzejewski@diweb.pl>
+ */
+class Variant extends BaseVariant
+{
+}
+
